76cad36b6eb9b0f79ede85d4b0b371a8eb3b3f20
basarballioz <basar.ballioz@decathlon.com>
2024-02-27 16:54:12 +0300

    meta name desc updated

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,7 @@ export default function Head() {
     <>
       <title>Konsolya - Gaming Excitement</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Personal Webiste Made With NextJS" />
+      <meta name="description" content="Konsolya, your go-to destination for gaming excitement at home" />
       <link rel="icon" href="/images/favicon.ico" />
       <link rel="apple-touch-icon" sizes="120x120" href="/images/favicon.ico" />
       <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon.ico" />
