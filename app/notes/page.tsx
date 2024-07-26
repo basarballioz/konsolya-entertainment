56ef324d9c6b4de21433bd3efec9bce614ded3b5
basarballioz <basar.ballioz@decathlon.com>
2024-07-26 12:37:29 +0300

    npm to pnpm

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -2,7 +2,8 @@ import { createClient } from '@/utils/supabase/server';
 
 export default async function Notes() {
     const supabase = createClient();
+    
     const { data: notes } = await supabase.from("notes").select();
 
-    return <pre>{JSON.stringify(notes, null, 2)}</pre>
+    return <pre>{JSON.stringify(notes, null, 3)}</pre>
 }
